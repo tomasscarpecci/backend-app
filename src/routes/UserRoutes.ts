181b632d87c7481d92880fc063a13f6bf1239732
tomasvuelta <tomasvuelta13@gmail.com>
2024-06-22 16:47:28 -0300

    mefaltoelid

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -9,6 +9,6 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
-router.delete('/id', userController.deleteUser);
+router.delete('/:id', userController.deleteUser);
 
 export default router;
